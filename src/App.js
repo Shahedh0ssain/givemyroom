db5d85ead4c90567fdf9bdf8ff427c3bb1fedf80
shahed hossain <shahed0hossain@gmail.com>
2023-11-16 11:35:59 +0600

    some issue solve

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React from "react";
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Components/pages/Home/Home';
-// import Order from './Components/pages/Order/Order';
-// import Products from './Components/pages/Products/Products';
 import Login from './Components/pages/Login/Login';
 import Register from './Components/pages/Register/Register'
-// import Requireauth from './Components/pages/Requireauth/Requireauth';
 import Footer from './Components/pages/Footer/Footer';
 import Navber from './Components/pages/Navber/Navber';
-// import Traslate from "./Components/Traslate/Traslate";
+
 
 
 
@@ -18,8 +15,7 @@ function App() {
   return (
     <div >
       <Navber></Navber>
-      
-      {/* <Header></Header> */}
+    
       <Routes>
         <Route path="/" element={<Home />}></Route>
         {/* <Route path="/products" element={<Products />}></Route> */}
