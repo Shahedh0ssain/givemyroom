314fb75f0d1d801903dbb754aa7f6e29bc994239
shahed hossain <shahed0hossain@gmail.com>
2023-11-16 11:33:46 +0600

    some issue solve

---
+++ src/Components/pages/Order/Order.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Products = () => {
+    return (
+        <div>
+            <h2 className='text-center text-primary mt-5'>Product</h2>
+         
+        </div>
+    );
+};
+
+export default Products;
